4983bbdc07df8bda08a3d9e7e22c227cbb83758e
Cheryl Casteling <cheryl@fanaticdesign.co.uk>
2017-10-26 16:42:00 +0100

    Added new snippet

---
+++ hide_until_window_loads.js
@@ -0,0 +1,11 @@
+// Taken from Pennethornes
+
+// hide until window loads
+$(window).bind("load", function() {
+    $(".c-book-now").css("display", "block");
+});
+
+// CSS for css file
+// .c-book-now {
+//   display: none;
+// }
